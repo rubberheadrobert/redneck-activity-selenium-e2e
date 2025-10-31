f9e670e549f9e67d26424616c6a5d29d5671fda4
rubberheadrobert <robert.csorgo@gmail.com>
2025-10-31 12:09:02 +0100

    Added screenshot functionality for failures

--- src/test/java/com/activity/base/BaseTest.java
+++ src/test/java/com/activity/base/BaseTest.java
@@ -1,12 +1,20 @@
 package com.activity.base;
 
 import com.activity.pages.HomePage;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
 
 import static com.activity.base.BasePage.delay;
 
@@ -39,5 +47,22 @@ public class BaseTest {
     public void tearDown(){
         delay(3000);
         driver.quit();
+    }
+
+    @AfterMethod
+    public void takeScreenshotOnFailure(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            TakesScreenshot ts = (TakesScreenshot) driver;
+            File src = ts.getScreenshotAs(OutputType.FILE);
+            String screenshotName = result.getMethod().getMethodName() + "_" + System.currentTimeMillis() + ".png";
+            File dest = new File("target/screenshots/" + screenshotName);
+            dest.getParentFile().mkdirs(); // make sure directory exists
+            try {
+                FileHandler.copy(src, dest);
+                System.out.println("Saved screenshot: " + dest.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
